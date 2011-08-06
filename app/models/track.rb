ebd81f9f4e6d88ab61bb29b9ac778a2f0729be74
Philip MacIver <philip.maciver@xing.com>
2011-08-06 13:39:57 +0200

    Moved indexing and caching to rake tasks

--- app/models/track.rb
+++ app/models/track.rb
@@ -15,8 +15,8 @@ class Track
       Track.all.destroy
       AppConfig.config[:index_dirs].each do |index_dir|
 
-        Dir[File.join(index_dir, "**", "*.mp3")].each do |file|
-          puts "Indexing #{file}"
+        Dir[File.join(index_dir, "**", "*.mp3")].each_with_index do |file, ind|
+          puts "#{ind + 1}. Indexing #{file}"
           Track.create :filename => file
         end
       end
